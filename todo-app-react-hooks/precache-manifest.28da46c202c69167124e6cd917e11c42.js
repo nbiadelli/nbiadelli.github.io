cf14f5eaa8cce94fc289a827cbbb69f4141e673d
Natalia Biadelli <natalia.biadelli@gmail.com>
2020-02-19 19:13:13 -0300

    Adicionado um projeto to-do com react hooks

---
+++ todo-app-react-hooks/precache-manifest.28da46c202c69167124e6cd917e11c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63525fd5f387621a808e6a732de4640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d16219d536b2e91d7b9",
+    "url": "/static/css/main.0803cab8.chunk.css"
+  },
+  {
+    "revision": "76eebd73bba1096a4d59",
+    "url": "/static/js/2.1b5f55e7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1b5f55e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d16219d536b2e91d7b9",
+    "url": "/static/js/main.b35fbcfe.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
